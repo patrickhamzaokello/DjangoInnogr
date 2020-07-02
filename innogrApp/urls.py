3acf02345b76d3b5b2385e4305c63d4b683116e6
Hafise <pkasemer@gmail.com>
2020-07-02 07:15:21 +0300

    firts commit

---
+++ innogrApp/urls.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.basepage, name='home'),
+    # path('globaldata/', views.globaldata, name='globaldata'),
+    # path('cov19Info/', views.wikipage, name='wikipage'),
+    path('about/', views.aboutpage, name='aboutpage'),
+    # path('news/', views.newspage, name='newspage'),
+
+
+]
---
+++ innogrApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import requests
+
+# Create your views here.
+
+
+def basepage(request):
+    return render(request, 'base.html')
+
+def aboutpage(request):
+    return render(request, 'about.html')
