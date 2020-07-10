121f15a9ec115d2865934c3e1a8c81fa20e03fdd
Pkasemer <pkasemer@gmail.com>
2020-07-10 17:25:54 +0300

    fixed media url

--- Innogr/settings.py
+++ Innogr/settings.py
@@ -130,6 +130,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 django_heroku.settings(locals())
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
