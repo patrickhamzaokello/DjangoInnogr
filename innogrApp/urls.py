01e0abef2747671d0b12807877926d4d6dfed854
Pkasemer <pkasemer@gmail.com>
2020-07-11 04:47:33 +0300

    post create, update and delete done

--- innogrApp/urls.py
+++ innogrApp/urls.py
@@ -1,16 +1,30 @@
 from django.contrib.auth import admin
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 from . import views
 
 urlpatterns = [
-    path('', views.basepage, name='blog-home'),
-    path('index.html', views.basepage, name='home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('index.html', PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+
+
+
     path('myDevices', views.mydevices, name='MyDevices'),
     path('newsFeed', views.newsFeed, name='MynewsFeed'),
     path('Profile/Overview', views.profilepage, name='profilepage'),
     path('Profile/Settings', views.Accountsettings, name='Accountsettings'),
     path('Resources/Support', views.Resources, name='Resources'),
-
 
 ]
--- innogrApp/views.py
+++ innogrApp/views.py
@@ -1,21 +1,64 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
+from django.views.generic import (
+    ListView, 
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from users.forms import UserRegistrationForm,UserUpdateForm,ProfileUpdateForm
-import requests
+
 
 # Create your views here.
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'innogrApp/index.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
-@login_required
-def basepage(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'innogrApp/index.html',context)
+class PostDetailView(DetailView):
+    model = Post
 
-@login_required
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
 def mydevices(request):
     return render(request,'innogrApp/pages/mydevices.html')
 
