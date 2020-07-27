0ef5e3fe43fb2966622d39ec9984e116ec4bc9eb
Hafise <pkasemer@gmail.com>
2020-07-27 10:22:36 +0300

    changed landing to root url

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.innogrhomepage, name='innogrhome'),
+    path('home/', views.innogrhomepage, name='innogrhome'),
     path('innogrlearn/', views.innogrlearn, name='innogrlearn'),
-    path('home/', views.landingpage, name='landingpage'),
+    path('', views.landingpage, name='landingpage'),
     
     
 ]
